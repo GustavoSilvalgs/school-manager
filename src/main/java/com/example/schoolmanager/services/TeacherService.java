2fe7a0b31fa755ecfb7c5ff4964c6b6476cdad81
GustavoSilvalgs <luisgust06@gmail.com>
2024-01-15 22:09:38 -0300

    feat: Add updateTeacher method to TeacherService
    Implemented the updateTeacher method in the TeacherService class to update existing Teacher entities. This method retrieves the existing entity based on RGM, updates the fields if provided, and saves the changes using TeacherRepository. If the entity is not found, an EntityNotFoundException is thrown.

--- src/main/java/com/example/schoolmanager/services/TeacherService.java
+++ src/main/java/com/example/schoolmanager/services/TeacherService.java
@@ -2,6 +2,7 @@ package com.example.schoolmanager.services;
 
 import com.example.schoolmanager.models.Teacher;
 import com.example.schoolmanager.repositories.TeacherRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +24,20 @@ public class TeacherService {
 
     public List<Teacher> getAllTeachers() {
         return repository.findAll();
+    }
+
+    public void updateTeacher(Teacher teacher) {
+        var entity = repository.findById(teacher.getRgm())
+                .orElseThrow(() -> new EntityNotFoundException("User with RGM " + teacher.getRgm() + " not found"));
+
+        if (teacher.getName() != null) {
+            entity.setName(teacher.getName());
+        }
+
+        if (teacher.getEmail() != null) {
+            entity.setEmail(teacher.getEmail());
+        }
+
+        repository.save(entity);
     }
 }
