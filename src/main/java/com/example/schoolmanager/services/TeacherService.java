8a9b1d1daf1b936c61f7f6a61fbba18c481c1147
GustavoSilvalgs <luisgust06@gmail.com>
2024-01-15 21:56:01 -0300

    feat: Add getAllTeachers method to TeacherService
    Implemented the getAllTeachers method in the TeacherService class to retrieve a list of all Teacher entities. This method utilizes the findAll method from TeacherRepository for data access.

--- src/main/java/com/example/schoolmanager/services/TeacherService.java
+++ src/main/java/com/example/schoolmanager/services/TeacherService.java
@@ -5,6 +5,8 @@ import com.example.schoolmanager.repositories.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TeacherService {
 
@@ -17,5 +19,9 @@ public class TeacherService {
 
     public Teacher getTeacherByRgm(Long rgm) {
         return repository.findById(rgm).orElse(null);
+    }
+
+    public List<Teacher> getAllTeachers() {
+        return repository.findAll();
     }
 }
