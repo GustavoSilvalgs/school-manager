a2ffe20ecaf6cf798462fb7cc1b18b1e7fd84e23
GustavoSilvalgs <luisgust06@gmail.com>
2024-01-15 21:33:46 -0300

    chore: Create TeacherRepository and StudentRepository interfaces
    Generated TeacherRepository and StudentRepository interfaces extending JpaRepository for Teacher and Student entities, respectively. These repositories provide basic CRUD operations and leverage the power of Spring Data JPA for data access.

---
+++ src/main/java/com/example/schoolmanager/repositories/TeacherRepository.java
@@ -0,0 +1,7 @@
+package com.example.schoolmanager.repositories;
+
+import com.example.schoolmanager.models.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
