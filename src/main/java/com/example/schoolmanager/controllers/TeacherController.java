a14657c44a174f5b943bba072765f5f3baa5380d
GustavoSilvalgs <luisgust06@gmail.com>
2024-01-16 18:29:09 -0300

    feat: Add getTeacherById endpoint to TeacherController
    Extended TeacherController with a GET endpoint ("/teacher/{id}") to retrieve a Teacher entity by its registration number (RGM). The endpoint delegates the operation to the TeacherService.

--- src/main/java/com/example/schoolmanager/controllers/TeacherController.java
+++ src/main/java/com/example/schoolmanager/controllers/TeacherController.java
@@ -3,10 +3,7 @@ package com.example.schoolmanager.controllers;
 import com.example.schoolmanager.models.Teacher;
 import com.example.schoolmanager.services.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/teacher")
@@ -18,5 +15,10 @@ public class TeacherController {
     @PostMapping
     public Teacher create(@RequestBody Teacher teacher) {
         return service.create(teacher);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Teacher getTeacherById(@PathVariable(value = "id") Long id) {
+        return service.getTeacherByRgm(id);
     }
 }
