41d0f51c6e1e97c8e7ea3085a2d34b4d84b11322
GustavoSilvalgs <luisgust06@gmail.com>
2024-01-24 00:35:05 -0300

    feat: add StudentDto class
    Added the StudentDto class with attributes for student information such as rgm, name, email, and registration date.

---
+++ src/main/java/com/example/schoolmanager/data/dto/StudentDto.java
@@ -0,0 +1,61 @@
+package com.example.schoolmanager.data.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class StudentDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rgm;
+    private String name;
+    private String email;
+    private LocalDate registrationDate;
+
+    public StudentDto() {
+    }
+
+    public StudentDto(Long rgm, String name, String email, LocalDate registrationDate) {
+        this.rgm = rgm;
+        this.name = name;
+        this.email = email;
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getRgm() {
+        return rgm;
+    }
+
+    public void setRgm(Long rgm) {
+        this.rgm = rgm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
--- src/main/java/com/example/schoolmanager/repositories/StudentRepository.java
+++ src/main/java/com/example/schoolmanager/repositories/StudentRepository.java
@@ -4,4 +4,6 @@ import com.example.schoolmanager.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    boolean existsByEmail(String email);
 }
