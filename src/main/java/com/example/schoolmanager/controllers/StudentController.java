325c0df6799c336b2affe26d566664e48695a77e
GustavoSilvalgs <luisgust06@gmail.com>
2024-01-25 20:12:45 -0300

    feat: add getStudentById endpoint to StudentController
    Implemented the `getStudentById` endpoint in the StudentController, allowing clients to retrieve student information by RGM. This enhances the API functionality, providing support for fetching individual student records based on their unique identifier.

--- src/main/java/com/example/schoolmanager/controllers/StudentController.java
+++ src/main/java/com/example/schoolmanager/controllers/StudentController.java
@@ -3,10 +3,7 @@ package com.example.schoolmanager.controllers;
 import com.example.schoolmanager.data.dto.StudentDto;
 import com.example.schoolmanager.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/student")
@@ -18,5 +15,10 @@ public class StudentController {
     @PostMapping
     public StudentDto create(@RequestBody StudentDto student) {
         return service.create(student);
+    }
+
+    @GetMapping(value = "/{id}")
+    public StudentDto getStudentById(@PathVariable(value = "id") Long id) {
+        return service.getStudentByRgm(id);
     }
 }
