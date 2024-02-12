b207ed92b1be7f7c88ade5bfb7758579a2ba3242
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-11 22:53:41 -0300

    test: Add unit tests for DozerMapper
    Introduce unit tests for the DozerMapper class to verify the correct conversion of entities to DTOs and entity lists to DTO lists.

---
+++ src/test/java/com/example/schoolmanager/unittests/mapper/DozerConverterTest.java
@@ -0,0 +1,57 @@
+package com.example.schoolmanager.unittests.mapper;
+
+import com.example.schoolmanager.data.dto.v1.TeacherDto;
+import com.example.schoolmanager.mapper.DozerMapper;
+import com.example.schoolmanager.models.Teacher;
+import com.example.schoolmanager.unittests.mapper.mocks.MockTeacher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DozerConverterTest {
+
+    MockTeacher inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockTeacher();
+    }
+
+    @Test
+    public void parseEntityToDTOTest() {
+        TeacherDto output = DozerMapper.parseObject(inputObject.mockEntity(), TeacherDto.class);
+        assertEquals(Long.valueOf(0L), output.getKey());
+        assertEquals("Name Test0", output.getName());
+        assertEquals("Email Test0", output.getEmail());
+        assertEquals(LocalDate.of(2024, 1, 1), output.getHiringDate());
+    }
+
+    @Test
+    public void parseEntityListToDTOListTest() {
+        List<TeacherDto> outputList = DozerMapper.parseListObjects(inputObject.mockEntityList(), TeacherDto.class);
+        TeacherDto outputZero = outputList.get(0);
+
+        assertEquals(Long.valueOf(0L), outputZero.getKey());
+        assertEquals("Name Test0", outputZero.getName());
+        assertEquals("Email Test0", outputZero.getEmail());
+        assertEquals(LocalDate.of(2024, 1, 1), outputZero.getHiringDate());
+
+        TeacherDto outputSeven = outputList.get(7);
+
+        assertEquals(Long.valueOf(7L), outputSeven.getKey());
+        assertEquals("Name Test7", outputSeven.getName());
+        assertEquals("Email Test7", outputSeven.getEmail());
+        assertEquals(LocalDate.of(2024, 1, 1), outputSeven.getHiringDate());
+
+        TeacherDto outputTwelve = outputList.get(12);
+
+        assertEquals(Long.valueOf(12L), outputTwelve.getKey());
+        assertEquals("Name Test12", outputTwelve.getName());
+        assertEquals("Email Test12", outputTwelve.getEmail());
+        assertEquals(LocalDate.of(2024, 1, 1), outputTwelve.getHiringDate());
+    }
+}
