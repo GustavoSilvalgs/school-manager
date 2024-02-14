18cc17d84fc3c4b3e9294ee76d94393c84d26749
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-14 20:13:13 -0300

    feat: Add API documentation for update endpoint
    Added Swagger annotations and responses descriptions for the update endpoint in the TeacherController. Specified the success and other possible status codes.

--- src/main/java/com/example/schoolmanager/controllers/TeacherController.java
+++ src/main/java/com/example/schoolmanager/controllers/TeacherController.java
@@ -28,7 +28,7 @@ public class TeacherController {
             consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
     @Operation(summary = "Adds a new Teacher",
-            description = "Adds a new Teacher by passing in a JSON, XML or YML representation of the person!",
+            description = "Adds a new Teacher by passing in a JSON, XML or YML representation of the teacher!",
             tags = {"Teacher"},
             responses = {
                     @ApiResponse(description = "Success", responseCode = "200",
@@ -86,6 +86,19 @@ public class TeacherController {
     @PutMapping(
             consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
             produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(summary = "Updates a Teacher",
+            description = "Updates a Teacher by passing in a JSON, XML or YML representation of the teacher!",
+            tags = {"Teacher"},
+            responses = {
+                    @ApiResponse(description = "Updated", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = TeacherDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public TeacherDto update(@RequestBody TeacherDto teacher) {
         return service.updateTeacher(teacher);
     }
