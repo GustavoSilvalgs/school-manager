8533bed5fa8497fcbeb467e97dc28a3e664d07f4
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-14 20:07:25 -0300

    feat: Add API documentation for getAllTeachers endpoint
    Added Swagger annotations and responses descriptions for the getAllTeachers endpoint in the TeachersController. Specified the supported media types for JSON, XML, and YAML.

--- src/main/java/com/example/schoolmanager/controllers/TeacherController.java
+++ src/main/java/com/example/schoolmanager/controllers/TeacherController.java
@@ -3,6 +3,13 @@ package com.example.schoolmanager.controllers;
 import com.example.schoolmanager.data.dto.v1.TeacherDto;
 import com.example.schoolmanager.services.TeacherService;
 import com.example.schoolmanager.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/teacher/v1")
+@Tag(name = "Teacher", description = "Endpoints for Managing People")
 public class TeacherController {
 
     @Autowired
@@ -30,6 +38,22 @@ public class TeacherController {
     }
 
     @GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(summary = "Finds all Teachers", description = "Finds all Teachers",
+            tags = {"Teachers"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = TeacherDto.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public List<TeacherDto> getAllTeachers() {
         return service.getAllTeachers();
     }
