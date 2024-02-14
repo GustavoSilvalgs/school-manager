d0a2670d2d515a289a609eb296a0da4884e0ebd5
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-14 20:14:23 -0300

    feat: Add API documentation for delete endpoint
    Added Swagger annotations and responses descriptions for the delete endpoint in the TeacherController. Specified the success and other possible status codes.

--- src/main/java/com/example/schoolmanager/controllers/TeacherController.java
+++ src/main/java/com/example/schoolmanager/controllers/TeacherController.java
@@ -104,6 +104,17 @@ public class TeacherController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deletes a Teacher",
+            description = "Deletes a Teacher by passing in a JSON, XML or YML representation of the teacher!",
+            tags = {"Teacher"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.deleteTeacher(id);
         return ResponseEntity.noContent().build();
