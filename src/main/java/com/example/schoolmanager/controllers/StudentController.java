80b756574d2f3a85c4f3825d9faac3357d8ffd3d
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-17 23:24:31 -0300

    feat: Add endpoint configuration for creating a new Student
    Added the configuration for the /api/student/v1 POST endpoint to create a new Student. Defined the request and response formats using Swagger annotations, specifying JSON, XML, and YML media types.

--- src/main/java/com/example/schoolmanager/controllers/StudentController.java
+++ src/main/java/com/example/schoolmanager/controllers/StudentController.java
@@ -2,6 +2,11 @@ package com.example.schoolmanager.controllers;
 
 import com.example.schoolmanager.data.dto.v1.StudentDto;
 import com.example.schoolmanager.services.StudentService;
+import com.example.schoolmanager.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +20,21 @@ public class StudentController {
     @Autowired
     private StudentService service;
 
-    @PostMapping
+    @PostMapping(
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(summary = "Adds a new Student",
+            description = "Adds a new Student by passing in a JSON, XML or YML representation of the student!",
+            tags = {"Student"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = StudentDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public StudentDto create(@RequestBody StudentDto student) {
         return service.create(student);
     }
