f807074ae550529e0cd2e2e4a1803e1d4793ddc8
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-17 23:29:38 -0300

    feat: Add endpoint configuration for retrieving a Student by ID
    Added the configuration for the /api/student/v1/{id} GET endpoint to retrieve a Student by ID. Defined the response formats using Swagger annotations, specifying JSON, XML, and YML media types.

--- src/main/java/com/example/schoolmanager/controllers/StudentController.java
+++ src/main/java/com/example/schoolmanager/controllers/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.schoolmanager.controllers;
 
 import com.example.schoolmanager.data.dto.v1.StudentDto;
+import com.example.schoolmanager.data.dto.v1.TeacherDto;
 import com.example.schoolmanager.services.StudentService;
 import com.example.schoolmanager.util.MediaType;
 import io.swagger.v3.oas.annotations.Operation;
@@ -39,7 +40,21 @@ public class StudentController {
         return service.create(student);
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}",
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(summary = "Finds a Student", description = "Finds a Student",
+            tags = {"Student"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = StudentDto.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public StudentDto getStudentById(@PathVariable(value = "id") Long id) {
         return service.getStudentByRgm(id);
     }
