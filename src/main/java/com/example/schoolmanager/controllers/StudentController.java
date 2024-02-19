52c150ffad63117cbd43b9ba3dff7a3b798dc8f7
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-18 23:52:13 -0300

    feat: Add endpoint for finding all students
    Added the endpoint to find all students in the StudentController. Included Swagger annotations for documentation. Implemented the getAllStudents method in the StudentService.

--- src/main/java/com/example/schoolmanager/controllers/StudentController.java
+++ src/main/java/com/example/schoolmanager/controllers/StudentController.java
@@ -5,6 +5,7 @@ import com.example.schoolmanager.data.dto.v1.TeacherDto;
 import com.example.schoolmanager.services.StudentService;
 import com.example.schoolmanager.util.MediaType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -59,7 +60,23 @@ public class StudentController {
         return service.getStudentByRgm(id);
     }
 
-    @GetMapping
+    @GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(summary = "Finds all Student", description = "Finds all Student",
+            tags = {"Student"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = StudentDto.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public List<StudentDto> getAllStudents() {
         return service.getAllStudents();
     }
