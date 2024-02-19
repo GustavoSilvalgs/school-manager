fb5f5e8bc4562e1716a88317760cad6c6037163e
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-18 23:57:00 -0300

    feat: Add endpoint for updating a student
    Added the endpoint to update a student in the StudentController. Included Swagger annotations for documentation. Implemented the updateStudent method in the StudentService.

--- src/main/java/com/example/schoolmanager/controllers/StudentController.java
+++ src/main/java/com/example/schoolmanager/controllers/StudentController.java
@@ -81,7 +81,22 @@ public class StudentController {
         return service.getAllStudents();
     }
 
-    @PutMapping
+    @PutMapping(
+            consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+            produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+    @Operation(summary = "Updates a Student",
+            description = "Updates a Student by passing in a JSON, XML or YML representation of the student!",
+            tags = {"Student"},
+            responses = {
+                    @ApiResponse(description = "Updated", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = StudentDto.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public StudentDto update(@RequestBody StudentDto student) {
         return service.updateStudent(student);
     }
