ef3d68e1bf6cbbc8bf127cf2cf11f922555760f6
GustavoSilvalgs <luisgust06@gmail.com>
2024-02-27 15:49:16 -0300

    feat: Add endpoint for deleting a student
    Added the endpoint to delete a student in the StudentController. Included Swagger annotations for documentation. Implemented the deleteStudent method in the StudentService.

--- src/main/java/com/example/schoolmanager/controllers/StudentController.java
+++ src/main/java/com/example/schoolmanager/controllers/StudentController.java
@@ -102,6 +102,17 @@ public class StudentController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deletes a Student",
+            description = "Deletes a Student by passing in a JSON, XML or YML representation of the student!",
+            tags = {"Student"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.deleteStudent(id);
         return ResponseEntity.noContent().build();
